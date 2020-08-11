530338377a53b6bc8e25b15b8493cbe806311ec4
Benson <dasilvabenson@gmail.com>
2020-08-10 19:37:38 -0900

    Loader Functionality added

--- QGEN/script.js
+++ QGEN/script.js
@@ -1,16 +1,29 @@
 console.log("QGEN IGNITED");
 //get Quote from API
-
+const quoteBox = document.getElementById("quote-container");
 const quoteBtn = document.getElementById("gen-newquote");
 const quoteAuthor = document.getElementById("author");
 const quoteText = document.getElementById("main-quote");
 const twtBtn = document.getElementById("tweet");
+const loader = document.getElementById("loader");
 
 const proxURL = "https://cors-anywhere.herokuapp.com/";
-const apiURL =
-  "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json";
+const apiURL = "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json";
 
+function loading() {
+  loader.hidden = false;
+  quoteBox.hidden = true;
+}
+
+function completecheck() {
+  if (!loader.hidden) {
+    loader.hidden = true;
+    quoteBox.hidden = false;
+  }
+}
+completecheck();
 async function getQuote() {
+  loading();
   try {
     const resp = await fetch(proxURL + apiURL);
     const data = await resp.json();
@@ -27,6 +40,8 @@ async function getQuote() {
     } else {
       quoteAuthor.innerText = data.quoteAuthor;
     }
+    //reset the check!
+    completecheck();
   } catch (err) {
     getQuote();
 
